ce494c64db683369982c04f5e714909f667799bc
Jacob Goodpipe <jacob.goodpipe@gmail.com>
2022-06-20 20:24:14 -0600

    Routed

--- team5/src/app/app-routing.module.ts
+++ team5/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from './pages/home/home.component';
+import {LoginComponent} from "./pages/login/login.component";
+import {SignUpComponent} from "./pages/sign-up/sign-up.component";
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignUpComponent }
 ];
 
 @NgModule({
