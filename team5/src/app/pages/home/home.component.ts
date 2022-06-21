1fa379df1a05e5da019a6bda0d8fc0f5af99405e
Jacob Goodpipe <jacob.goodpipe@gmail.com>
2022-06-21 04:56:17 -0600

    home

--- team5/src/app/pages/home/home.component.ts
+++ team5/src/app/pages/home/home.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  price = '20';
+  credits = '';
+  amount = '';
+  volume = '';
+  dailyRange = '';
+  longRange = '';
 
   constructor() { }
 
