dfd68621c494a06067c328a0db7e334d0a9d7eb5
CA\mromanow <markus_romanow@hotmail.com>
2022-06-21 09:30:26 -0600

    buy page

--- team5/src/app/pages/buy/buy.component.ts
+++ team5/src/app/pages/buy/buy.component.ts
@@ -1,3 +1,5 @@
+import { ElementRef } from '@angular/core';
+import { ViewChild } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuyComponent implements OnInit {
 
+  @ViewChild('marketPrice') marketPrice: ElementRef
+  @ViewChild('numOfUnits') numOfUnits: ElementRef
+  @ViewChild('tradeFee') tradeFee: ElementRef
+  @ViewChild('totalAmount') totalAmount: ElementRef 
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+  
+  calcTotal(){  
+    var marketPrice = this.marketPrice.nativeElement.value;
+    var numOfUnits = this.numOfUnits.nativeElement.value;
+    var tradeFee = 5.75;
+
+    var total = (marketPrice * numOfUnits) + tradeFee;
+
+    this.totalAmount.nativeElement.value = total;
   }
 
 }
