ed42ff18c7faaf7ac0f288dceedb16ad8333418f
CA\mromanow <markus_romanow@hotmail.com>
2022-06-21 10:01:08 -0600

    no message

--- team5/src/app/pages/sell/sell.component.ts
+++ team5/src/app/pages/sell/sell.component.ts
@@ -1,3 +1,5 @@
+import { ElementRef } from '@angular/core';
+import { ViewChild } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,11 +7,27 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './sell.component.html',
   styleUrls: ['./sell.component.css']
 })
-export class SellComponent implements OnInit {
+export class BuyComponent implements OnInit {
+
+  @ViewChild('marketPrice') marketPrice: ElementRef
+  @ViewChild('numOfUnits') numOfUnits: ElementRef
+  @ViewChild('tradeFee') tradeFee: ElementRef
+  @ViewChild('totalAmount') totalAmount: ElementRef 
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  
+  calcTotal(){  
+    var marketPrice = this.marketPrice.nativeElement.value;
+    var numOfUnits = this.numOfUnits.nativeElement.value;
+    var tradeFee = 5.75;
+
+    var total = (marketPrice * numOfUnits) + tradeFee;
+
+    this.totalAmount.nativeElement.value = total;
   }
 
 }
