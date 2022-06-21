b5bfadbb48b79d15b9d670faae6624bcd9fb8545
Jacob Goodpipe <jacob.goodpipe@gmail.com>
2022-06-20 21:10:30 -0600

    Buy and Sell component

--- team5/src/app/app-routing.module.ts
+++ team5/src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from './pages/home/home.component';
 import {LoginComponent} from "./pages/login/login.component";
 import {SignUpComponent} from "./pages/sign-up/sign-up.component";
+import {SellComponent} from "./pages/sell/sell.component";
+import {BuyComponent} from "./pages/buy/buy.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'signup', component: SignUpComponent }
+  { path: 'signup', component: SignUpComponent },
+  { path: 'buy', component: BuyComponent },
+  { path: 'sell', component: SellComponent }
 ];
 
 @NgModule({
--- team5/src/app/app.module.ts
+++ team5/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { BuyComponent } from './pages/buy/buy.component';
+import { SellComponent } from './pages/sell/sell.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    SignUpComponent
+    SignUpComponent,
+    BuyComponent,
+    SellComponent
   ],
   imports: [
     BrowserModule,
